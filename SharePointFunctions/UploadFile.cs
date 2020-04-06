b5d59aa56e3fb90160a6f99a7e0715a8969b7e65
Thomas Kliche <office@klichedigital.at>
2020-04-06 13:50:35 +0200

    Add project files.

---
+++ SharePointFunctions/UploadFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using System.IO;
+
+namespace SharePointFunctions
+{
+    public class UploadFile
+    {
+        public string SiteUrl { get; set; }
+        public string DocumentLibrary { get; set; }
+        public string FileName { get; set; }
+        public string Folder { get; set; }
+        public string UserName { get; set; }
+
+        private SecureString SecPassword;
+        public string Password {
+            set
+            {
+                SecureString securePassword = new SecureString();
+                foreach (char c in value)
+                { securePassword.AppendChar(c); }
+                this.SecPassword = securePassword;
+            }
+        }
+
+
+        public void StartUpload()
+        {
+            UploadFileToSharePoint();
+        }
+
+        private void UploadFileToSharePoint()
+        {
+            SharePointOnlineCredentials onlineCredentials = new SharePointOnlineCredentials(this.UserName, this.SecPassword);
+            using (ClientContext CContext = new ClientContext(SiteUrl))
+            {
+                CContext.Credentials = onlineCredentials;
+                Web web = CContext.Web;
+                FileCreationInformation newFile = new FileCreationInformation();
+                byte[] FileContent = System.IO.File.ReadAllBytes(this.FileName);
+                newFile.ContentStream = new MemoryStream(FileContent);
+                newFile.Url = Path.GetFileName(this.FileName);
+                newFile.Overwrite = true;
+                List DocumentLibrary = web.Lists.GetByTitle(this.DocumentLibrary);
+                //SP.Folder folder = DocumentLibrary.RootFolder.Folders.GetByUrl(ClientSubFolder);
+                Folder Clientfolder = DocumentLibrary.RootFolder.Folders.Add(this.Folder);
+                Clientfolder.Update();
+                Microsoft.SharePoint.Client.File uploadFile = Clientfolder.Files.Add(newFile);
+
+                CContext.Load(DocumentLibrary);
+                CContext.Load(uploadFile);
+                CContext.ExecuteQuery();
+            }
+        }
+    }
+}
